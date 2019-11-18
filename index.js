89b08dbdabac37008ce100ede800a97909fa5fac
Eric Penn <ep00959@georgiasouthern.edu>
2019-11-18 07:05:16 -0500

    final edit

---
+++ index.js
@@ -0,0 +1,5 @@
+$('a.jslink').click(function(two) {
+    two.preventDefault();
+    window.open("https://validator.w3.org/#validate_by_input");
+    window.open("https://jigsaw.w3.org/css-validator/validator");
+});
